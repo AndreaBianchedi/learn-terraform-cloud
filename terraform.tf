3c67854a29112417b8612cdd6d03b461cf3c8cec
AndreaBianchedi <andreabianchedi@yahoo.it>
2024-02-21 16:56:18 +0100

    Remove cloud block

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+/*
+  cloud {
+    organization = "FEDERICO_BIANCHEDI"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
